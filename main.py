4c19d394c9be7ae2edf888b31570529a8cebe2e5
ulivanlrn <kuryzhde@gmail.com>
2025-06-11 23:12:05 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
+# import pandas as pd
 from data.load_data import load_data
 from preprocessing.impute_data import fill_in_constant
 from preprocessing.impute_data import random_impute
@@ -10,10 +10,10 @@ with open("config/baseline_config.yaml", "r") as f:
 
 # data loading
 data = load_data(config["data"]["path"])
+
 # cleaning data
-data = data.drop(['company'], axis=1)
+data = data.drop(config["preprocessing"]["drop_from_start"], axis=1)
 data = data[data['adults']>0]
-data['reservation_status_date'] = pd.to_datetime(data['reservation_status_date'])
 data['children'] = fill_in_constant(data['children'],
                                     config["preprocessing"]["children_impute_value"])
 random_impute(data, 'country')
