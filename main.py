0cb5d99878fe81eca552a431722b91f3668bc115
ulivanlrn <kuryzhde@gmail.com>
2025-06-12 11:17:24 +0200

    minor changes

--- main.py
+++ main.py
@@ -11,10 +11,25 @@ with open("config/baseline_config.yaml", "r") as f:
 # data loading
 data = load_data(config["data"]["path"])
 
-# cleaning data
+# initial cleaning
 data = data.drop(config["preprocessing"]["drop_from_start"], axis=1)
 data = data[data['adults']>0]
+
+# imputing missing values
 data['children'] = fill_in_constant(data['children'],
                                     config["preprocessing"]["children_impute_value"])
 random_impute(data, 'country')
 random_impute(data, 'agent')
+
+#feature engineering
+init_columns = list(data.columns)
+feature_flags = config["features"]["flags"]
+# if feature_flags["total_nights"] & ''
+# 'total_nights',
+#  'stays_format',
+#  'total_guests',
+#  'room_assigned_equal_reserved',
+#  'deposit_type',
+#  'trend',
+#  'seasonal',
+#  'resid'
