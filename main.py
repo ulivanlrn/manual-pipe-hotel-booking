a5d05a050ef39696dc529f28cd9d285aa291a1d5
ulivanlrn <kuryzhde@gmail.com>
2025-06-14 15:47:01 +0200

    added train_test_split

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # import pandas as pd
+from sklearn.model_selection import train_test_split
 from data.load_data import load_data
 from preprocessing.feature_engineering import run_feature_engineering
 from preprocessing.impute_data import run_imputation
@@ -14,7 +15,13 @@ data = load_data(config["data"]["path"])
 # initial cleaning
 data = data[data['adults']>0]
 data = data.drop(config["preprocessing"]["drop_from_start"], axis=1)
-current_features = set(data.columns)
+
+# splitting data
+X = data.drop(['is_canceled'], axis=1)
+y = data['is_canceled']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42,
+                                                    stratify=y)
+current_features = set(X_train.columns)
 
 # imputing missing values
 data = run_imputation(data, config, current_features)
