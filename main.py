472e2dd74652c0e7709e5a67cde27874fcaeb8d0
ulivanlrn <kuryzhde@gmail.com>
2025-06-14 12:55:56 +0200

    refactored feature_engineering section

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 from data.load_data import load_data
 from preprocessing.impute_data import fill_in_constant
 from preprocessing.impute_data import random_impute
-from preprocessing.feature_engineering import stays_func
-from preprocessing.feature_engineering import room_type
-from preprocessing.feature_engineering import deposit_type
-from utils import set1_in_set2
+from preprocessing.feature_engineering import run_feature_engineering
 
 # opening config
 import yaml
@@ -30,33 +27,8 @@ for feature in ['country', 'agent']:
         random_impute(data, feature)
 
 # feature engineering
-feature_flags = config["features"]["flags"]
-# a variable that collects all old features which contributed to new ones
-# and are not needed afterward
-drop_after_fe = set()
-
-if feature_flags["total_nights"] &\
-    set1_in_set2({'stays_in_weekend_nights', 'stays_in_week_nights'}, current_features):
-    data['total_nights'] = data['stays_in_weekend_nights'] + data['stays_in_week_nights']
-    drop_after_fe = drop_after_fe.union({'stays_in_weekend_nights', 'stays_in_week_nights'})
-
-if feature_flags["stays_format"] &\
-    set1_in_set2({'stays_in_weekend_nights', 'stays_in_week_nights'}, current_features):
-    data['stays_format'] = data.apply(stays_func, axis=1)
-
-if feature_flags["total_guests"] &\
-    set1_in_set2({'adults', 'children', 'babies'}, current_features):
-    data['total_guests'] = data['adults'] + data['children'] + data['babies']
-    drop_after_fe = drop_after_fe.union({'adults', 'children', 'babies'})
-
-if feature_flags["room_assigned_equal_reserved"] &\
-    set1_in_set2({'assigned_room_type', 'reserved_room_type'}, current_features):
-    data['room_assigned_equal_reserved'] = data.apply(room_type, axis=1)
-    drop_after_fe = drop_after_fe.union({'assigned_room_type', 'reserved_room_type'})
-
-if feature_flags["map_deposit_type"]:
-    data['deposit_type'] = data.apply(deposit_type, axis=1)
-
-data = data.drop(drop_after_fe, axis=1)
+data = run_feature_engineering(data, config, current_features)
 
 current_features = set(data.columns)
+print(len(current_features))
+print(current_features)
--- preprocessing/feature_engineering.py
+++ preprocessing/feature_engineering.py
@@ -1,3 +1,5 @@
+from utils import set1_in_set2
+
 def stays_func(row):
     feature1 = 'stays_in_weekend_nights'
     feature2 = 'stays_in_week_nights'
@@ -22,3 +24,35 @@ def deposit_type(row):
         return 1
     else:
         return 0
+
+def run_feature_engineering(data, config, current_features):
+    feature_flags = config["features"]["flags"]
+    # a variable that collects all old features which contributed to new ones \
+    # and are not needed afterward
+    drop_after_fe = set()
+
+    if feature_flags["total_nights"] & \
+            set1_in_set2({'stays_in_weekend_nights', 'stays_in_week_nights'}, current_features):
+        data['total_nights'] = data['stays_in_weekend_nights'] + data['stays_in_week_nights']
+        drop_after_fe = drop_after_fe.union({'stays_in_weekend_nights', 'stays_in_week_nights'})
+
+    if feature_flags["stays_format"] & \
+            set1_in_set2({'stays_in_weekend_nights', 'stays_in_week_nights'}, current_features):
+        data['stays_format'] = data.apply(stays_func, axis=1)
+
+    if feature_flags["total_guests"] & \
+            set1_in_set2({'adults', 'children', 'babies'}, current_features):
+        data['total_guests'] = data['adults'] + data['children'] + data['babies']
+        drop_after_fe = drop_after_fe.union({'adults', 'children', 'babies'})
+
+    if feature_flags["room_assigned_equal_reserved"] & \
+            set1_in_set2({'assigned_room_type', 'reserved_room_type'}, current_features):
+        data['room_assigned_equal_reserved'] = data.apply(room_type, axis=1)
+        drop_after_fe = drop_after_fe.union({'assigned_room_type', 'reserved_room_type'})
+
+    if feature_flags["map_deposit_type"]:
+        data['deposit_type'] = data.apply(deposit_type, axis=1)
+
+    data.drop(drop_after_fe, axis=1, inplace=True)
+
+    return data
