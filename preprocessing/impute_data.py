bbb7d064bd314396d005889fbce77da21d3ae2b0
ulivanlrn <kuryzhde@gmail.com>
2025-06-15 14:39:21 +0200

    finished Imputer class

--- preprocessing/impute_data.py
+++ preprocessing/impute_data.py
@@ -3,39 +3,43 @@ from sklearn.impute import KNNImputer
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 
-def fill_in_constant(column, value):
-    return column.fillna(value)
-
-def random_impute(df, feature):
-    random_sample = df[feature].dropna().sample(df[feature].isna().sum())
-    random_sample.index = df[df[feature].isna()].index
-    df.loc[df[feature].isna(), feature] = random_sample
-
-def run_imputation(data, config, current_features):
-    if 'children' in current_features:
-        data['children'] = fill_in_constant(data['children'],
-                                            config["preprocessing"]["children_impute_value"])
-    # NOTE: directly depends on the sample
-    for feature in ['country', 'agent']:
-        if feature in current_features:
-            random_impute(data, feature)
-
-    return data
-
 class Imputer(BaseEstimator, TransformerMixin):
+    """
+    A custom general imputer, which applies KNNImputer to numerical features and
+    SimpleImputer to categorical features. Column 'children' is handled separately.
+
+    Attributes:
+        config (dict): Configuration dictionary.
+        num_features (list): List of numerical features.
+        cat_features (list): List of categorical features.
+    """
     def __init__(self, config, num_features, cat_features):
         self.config = config
         self.num_features = num_features
         self.cat_features = cat_features
-        self.knn_neighbors = config["preprocessing"]["knn_neighbors"]
+        self.knn_neighbors = config["preprocessing"]["knn_imputer_neighbors"]
         self.knn_imputer = KNNImputer(n_neighbors=self.knn_neighbors)
         self.simple_imputer = SimpleImputer(strategy='most_frequent')
         self.column_transformer = ColumnTransformer(
             transformers=[
-
-            ]
+                ('knn_imputer', self.knn_imputer, self.num_features),
+                ('simple_imputer', self.simple_imputer, self.cat_features)
+            ],
+            remainder='passthrough',
+            verbose_feature_names_out=False
         )
+    @staticmethod
+    def fill_in_constant(column, value):
+        return column.fillna(value)
+
     def fit(self, x):
-        self.knn_imputer.fit(x)
+        self.column_transformer.fit(x)
+        return self
+
     def transform(self, x):
-        pass
+        x_copy = x.copy()
+        if 'children' in self.num_features:
+            x_copy['children'] = self.fill_in_constant(x_copy['children'],
+                                        self.config["preprocessing"]["children_impute_value"])
+        x_transformed = self.column_transformer.transform(x_copy)
+        return x_transformed
