516fe1fa6a1713eae1bd99e3e64caf77279c8150
ulivanlrn <kuryzhde@gmail.com>
2025-06-15 14:39:50 +0200

    made some changes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-# import pandas as pd
+from sklearn import set_config
 from sklearn.model_selection import train_test_split
 from data.load_data import load_data
 from preprocessing.feature_engineering import run_feature_engineering
-from preprocessing.impute_data import run_imputation
+from preprocessing.impute_data import Imputer
+
+set_config(transform_output='pandas')
 
 # opening config
 import yaml
@@ -19,20 +21,22 @@ data = data.drop(config["preprocessing"]["drop_from_start"], axis=1)
 # splitting data
 X = data.drop(['is_canceled'], axis=1)
 y = data['is_canceled']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42,
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=123,
                                                     stratify=y)
 
-cat_features = X.select_dtypes('object').columns
-num_features = X.select_dtypes(exclude='object').columns
+cat_features = list(X.select_dtypes('object').columns)
+num_features = list(X.select_dtypes(exclude='object').columns)
 
 current_features = set(X.columns)
 
 # imputing missing values
-data = run_imputation(data, config, current_features)
+imputer = Imputer(config, num_features, cat_features)
+X_train_imputed = imputer.fit_transform(X_train)
+X_test_imputed = imputer.transform(X_test)
 
 # feature engineering
-data = run_feature_engineering(data, config, current_features)
-
-current_features = set(data.columns)
+# data = run_feature_engineering(data, config, current_features)
+#
+# current_features = set(data.columns)
 # print(len(current_features))
 # print(current_features)
