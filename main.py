7fd127e45ec6104536204471928b8afe410c0557
ulivanlrn <kuryzhde@gmail.com>
2025-06-19 15:58:00 +0200

    finished feature engineering section

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from sklearn import set_config
 from sklearn.model_selection import train_test_split
 from data.load_data import load_data
 from preprocessing.feature_engineering import run_feature_engineering
 from preprocessing.impute_data import Imputer
-
+from sklearn import set_config
 set_config(transform_output='pandas')
 
 # opening config
@@ -26,17 +25,18 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 
 cat_features = list(X.select_dtypes('object').columns)
 num_features = list(X.select_dtypes(exclude='object').columns)
-
 current_features = set(X.columns)
 
 # imputing missing values
 imputer = Imputer(config, num_features, cat_features)
-X_train_imputed = imputer.fit_transform(X_train)
-X_test_imputed = imputer.transform(X_test)
+X_train = imputer.fit_transform(X_train)
+X_test = imputer.transform(X_test)
 
 # feature engineering
-# data = run_feature_engineering(data, config, current_features)
-#
-# current_features = set(data.columns)
-# print(len(current_features))
-# print(current_features)
+X_train = run_feature_engineering(X_train, config, current_features)
+X_test = run_feature_engineering(X_test, config, current_features)
+
+current_features = set(X_train.columns)
+print(X_train.dtypes.value_counts())
+print(len(current_features))
+print(current_features)
--- preprocessing/feature_engineering.py
+++ preprocessing/feature_engineering.py
@@ -26,33 +26,43 @@ def deposit_type(row):
         return 0
 
 def run_feature_engineering(data, config, current_features):
+    """
+    Run feature engineering on the given data.
+    :param data: Dataframe.
+    :param config: Configuration file.
+    :param current_features: The features which are currently in the data.
+    :return: Dataframe enriched with the new features.
+    """
+    df = data.copy()
+
+    # A dictionary specifying which features should be created
     feature_flags = config["features"]["flags"]
+
     # a variable that collects all old features which contributed to new ones \
     # and are not needed afterward
-    drop_after_fe = set()
+    feats_to_drop = set()
 
     if feature_flags["total_nights"] & \
             set1_in_set2({'stays_in_weekend_nights', 'stays_in_week_nights'}, current_features):
-        data['total_nights'] = data['stays_in_weekend_nights'] + data['stays_in_week_nights']
-        drop_after_fe = drop_after_fe.union({'stays_in_weekend_nights', 'stays_in_week_nights'})
+        df['total_nights'] = df['stays_in_weekend_nights'] + df['stays_in_week_nights']
+        feats_to_drop = feats_to_drop.union({'stays_in_weekend_nights', 'stays_in_week_nights'})
 
     if feature_flags["stays_format"] & \
             set1_in_set2({'stays_in_weekend_nights', 'stays_in_week_nights'}, current_features):
-        data['stays_format'] = data.apply(stays_func, axis=1)
+        df['stays_format'] = df.apply(stays_func, axis=1)
 
     if feature_flags["total_guests"] & \
             set1_in_set2({'adults', 'children', 'babies'}, current_features):
-        data['total_guests'] = data['adults'] + data['children'] + data['babies']
-        drop_after_fe = drop_after_fe.union({'adults', 'children', 'babies'})
+        df['total_guests'] = df['adults'] + df['children'] + df['babies']
+        feats_to_drop = feats_to_drop.union({'adults', 'children', 'babies'})
 
     if feature_flags["room_assigned_equal_reserved"] & \
             set1_in_set2({'assigned_room_type', 'reserved_room_type'}, current_features):
-        data['room_assigned_equal_reserved'] = data.apply(room_type, axis=1)
-        drop_after_fe = drop_after_fe.union({'assigned_room_type', 'reserved_room_type'})
+        df['room_assigned_equal_reserved'] = df.apply(room_type, axis=1)
+        feats_to_drop = feats_to_drop.union({'assigned_room_type', 'reserved_room_type'})
 
     if feature_flags["map_deposit_type"]:
-        data['deposit_type'] = data.apply(deposit_type, axis=1)
+        df['deposit_type'] = df.apply(deposit_type, axis=1)
 
-    data.drop(drop_after_fe, axis=1, inplace=True)
-
-    return data
+    df.drop(feats_to_drop, axis=1, inplace=True)
+    return df
