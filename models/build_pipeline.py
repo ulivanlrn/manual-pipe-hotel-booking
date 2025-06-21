f1882178bc41c02720560ad0c58d904967388111
ulivanlrn <kuryzhde@gmail.com>
2025-06-21 15:44:21 +0200

    finished encoding step

--- models/build_pipeline.py
+++ models/build_pipeline.py
@@ -1,10 +1,15 @@
-import logging
+# import logging
 from sklearn.pipeline import Pipeline
 from preprocessing.feature_encoding import Encoder
 
 def build_pipeline(model_config):
     steps = []
-    if model_config["needs_encoding"]:
-        steps.append(('encoder', Encoder()))
-    logging.info("check")
+
+    # encoding
+    if model_config["encoding"]["requires_encoding"]:
+        random_state = model_config["encoding"]["random_state"]
+        card_threshold = model_config["encoding"]["cardinality_threshold"]
+        encoder = Encoder(random_state=random_state, threshold=card_threshold)
+        steps.append(('encoder', encoder))
+
     return Pipeline(steps)
