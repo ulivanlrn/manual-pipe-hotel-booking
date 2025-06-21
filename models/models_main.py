7b9a6dff440958fe4fa3dc3e0c1bdfa12cb90d8e
ulivanlrn <kuryzhde@gmail.com>
2025-06-21 12:57:44 +0200

    added logging of initial steps, added building pipeline step

--- models/models_main.py
+++ models/models_main.py
@@ -1,4 +1,5 @@
 from data.load_data import load_data
+from models.build_pipeline import build_pipeline
 import yaml
 from sklearn import set_config
 import logging
@@ -6,13 +7,17 @@ import logging
 set_config(transform_output='pandas')
 logging.basicConfig(level=logging.DEBUG, filename='../logs/models.log', filemode='w')
 
-# opening model config
+# MODEL AND CONFIG
+model_type = "logistic_regression"
 config_name = "baseline"
-config_path = f"../config/logistic_regression/{config_name}.yaml"
+config_path = f"../config/{model_type}/{config_name}.yaml"
 with open(config_path, "r") as f:
     config = yaml.safe_load(f)
 
-# data loading (data name suffix is the same as the name of the config for preprocessing,
+logging.info(f"Experiment on: {model_type}")
+logging.info(f"Config: {config_name}")
+
+# DATA LOADING (data name suffix is the same as the name of the config for preprocessing,
 # which was used to create a specific partition)
 preprocessing_config_name = "baseline"
 
@@ -21,6 +26,9 @@ X_test = load_data(f"../data/X_test_{preprocessing_config_name}.csv")
 y_train = load_data(f"../data/y_train_{preprocessing_config_name}.csv")
 y_test = load_data(f"../data/y_test_{preprocessing_config_name}.csv")
 
-logging.info("Loaded train, test and target data")
+logging.info(f"Data version: {preprocessing_config_name}")
+logging.info("Data loading complete")
 
 # building pipeline
+pipeline = build_pipeline(config)
+logging.debug(pipeline.steps)
