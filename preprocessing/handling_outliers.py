1ae240e205bec215aea27670c1f3999a3cbbed44
ulivanlrn <kuryzhde@gmail.com>
2025-06-22 02:17:33 +0200

    started with __init__ method

--- preprocessing/handling_outliers.py
+++ preprocessing/handling_outliers.py
@@ -1,9 +1,21 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import FunctionTransformer
+import numpy as np
 
+def log_transform(df):
+    for col in df:
+        df[col] = np.log1p(df[col])
+    return df.fillna(0)
 
 class OutlierHandler(BaseEstimator, TransformerMixin):
 
-    def __init__(self):
+    def __init__(self, outlier_config):
+        self.outlier_config = outlier_config
+        self.transformers = []
+        if self.outlier_config["log_transform"]["flag"]:
+            self.log_transform = ('log_transform', FunctionTransformer(),
+                                  self.outlier_config["log_transform"]["columns"])
         pass
 
     def fit(self, x, y=None):
