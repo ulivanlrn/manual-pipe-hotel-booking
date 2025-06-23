af8c493a400a3e0e247a547bd00ded5dfead06f8
ulivanlrn <kuryzhde@gmail.com>
2025-06-23 14:35:59 +0200

    added scaling section

--- models/build_pipeline.py
+++ models/build_pipeline.py
@@ -1,6 +1,7 @@
 # import logging
 from sklearn.pipeline import Pipeline
 from preprocessing.feature_encoding import Encoder
+from preprocessing.feature_scaling import Scaler
 from preprocessing.handling_outliers import OutlierHandler
 
 def build_pipeline(model_config):
@@ -17,5 +18,11 @@ def build_pipeline(model_config):
         outlier_config = model_config["outliers"]
         outlier_handler = OutlierHandler(outlier_config=outlier_config)
         steps.append(('outlier_handler', outlier_handler))
+
+    # scaling
+    if model_config["scaling"]["flag"]:
+        scaling_config = model_config["scaling"]
+        scaler = Scaler(scaling_config=scaling_config)
+        steps.append(('scaler', scaler))
 
     return Pipeline(steps)
